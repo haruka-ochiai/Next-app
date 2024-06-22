6602918bf36c86f5446dcf30f0dd877641524216
haruka-ochiai <pw.g1.po1122@gmail.com>
2024-06-22 17:25:18 +0900

    add page

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About ページ</h1>
+            <h2>ここは馬好きによる馬好きのためのサイトです</h2>
+        </div>
+    )
+}
+
+export default AboutPage;
---
+++ src/app/login/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>ログインページ</h1>
+            <h2>ログイン方法を選択する</h2>
+        </div>
+    )
+}
+
+export default LoginPage;
