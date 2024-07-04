5c1094f272411e8181e9d658b8544d435b103b27
haruka-ochiai <pw.g1.po1122@gmail.com>
2024-07-04 22:29:43 +0900

    fix design

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -48,7 +48,51 @@ const SignUpPage = () => {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div>
-                <h1>新規登録</h1>
+                <h1 className="mb-4">新規登録</h1>
+                <form onSubmit= { handleSubmit } className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">ユーザー名：</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            value={ formData.username }
+                            onChange={ handleChange }
+                            required
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">メールアドレス：</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={ formData.email }
+                            onChange={ handleChange }
+                            required
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">パスワード：</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={ formData.password }
+                            onChange={ handleChange }
+                            required
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                    </div>
+                    <div >
+                    <button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        >登録</button>
+                    </div>
+                </form>
             </div>
         </main>
     )
